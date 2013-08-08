8cb62cfd0d0d0300860b154df0ffaa02c7b99b12
david <simpleaznx@gmail.com>
2013-08-08 01:30:28 -0700

    pin model move def image_remote_url above validated_attachment

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,16 +4,18 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "320x240" }
 	validates :description, presence: true
 	validates :user_id, presence: true
+
+
+	def image_remote_url=(url_value)
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
+  end
+  
 	validates_attachment :image, presence: true,
 									content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
 									size: { less_than: 3.megabytes }		
 
 	belongs_to :user
   
-
-	def image_remote_url=(url_value)
-    self.image = URI.parse(url_value) unless url_value.blank?
-    super
-  end
   
 end
