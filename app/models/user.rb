c40f340313357b4171f4229cd707ef0fdeeab94b
david <simpleaznx@gmail.com>
2013-08-09 20:13:29 -0700

    setup confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  validates_uniqueness_of :username
   devise :database_authenticatable, :registerable, #:recoverable,
-          :rememberable, :trackable, :validatable
+          :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :user_name, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name
   # attr_accessible :title, :body
 
   has_many :pins, :dependent => :destroy
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Pinapp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -37,4 +37,19 @@ Pinapp::Application.configure do
   
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "mail.google.com",
+  authentication: "plain",
+  # enable_starttls_auto: false,
+  :enable_starttls_auto => true,
+  # :openssl_verify_mode  => 'none',
+  user_name: ENV['SMTP_USERNAME'],
+  password: ENV['SMTP_PASSWORD']
+}
+
 end
